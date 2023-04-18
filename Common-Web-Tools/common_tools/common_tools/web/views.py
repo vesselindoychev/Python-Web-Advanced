c689570b41d042e0007403d8b1687d1cf5164038
Veselin Doychev <veselindoi123@gmail.com>
2023-04-18 15:24:08 +0300

    Added Session

--- Common-Web-Tools/common_tools/common_tools/web/views.py
+++ Common-Web-Tools/common_tools/common_tools/web/views.py
@@ -5,13 +5,24 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
 
-@cache_page(30)
+# @cache_page(30)
 def show_home_index(request):
     if not cache.get('value2'):
         cache.set('value2', random.randint(1, 1024), 30)
+
+    count = request.session.get('count') or 0
+    request.session['count'] = count + 1
+
     context = {
         'value2': random.randint(1, 1024),
         'value': random.randint(1, 1024),
+        'count': request.session.get('count'),
     }
 
     return render(request, 'index.html', context)
+
+
+def show_book_details(request, pk):
+    last_viewed = request.session.get('last_viewed_books') or []
+    last_viewed.append(pk)
+    request.session['last_viewed_books'] = last_viewed
